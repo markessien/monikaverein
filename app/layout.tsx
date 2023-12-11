4d32de01f86b469f0151342c8b3f66ae2e74564f
Justice <sherllyj191@gmail.com>
2023-12-11 20:07:55 +0100

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Oswald } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Header from "./widgets/header";
 import Footer from "./widgets/footer";
@@ -21,16 +22,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
   return (
     <html lang="en" data-theme="light">
-      {/* <head>
-        
-      </head> */}
-
+      <head>
+        <title>Monika Verein</title>
+      </head>
       <body className={`${font} bg-white-f9`}>
         <Header />
+
         {children}
+
         <Footer />
 
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
