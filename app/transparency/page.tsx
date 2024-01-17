070c51b19cc196dc5329cafc63d0a06f591ac308
Justice <sherllyj191@gmail.com>
2024-01-17 10:02:49 +0100

    Added transparente to transparency page

--- app/transparency/page.tsx
+++ app/transparency/page.tsx
@@ -1,10 +1,12 @@
 import ContactInfo from "@/shared/components/ContactInfo";
 import Waitlist from "@/shared/components/Waitlist";
 import Content from "./content";
+import TransparentSociety from "./components/TransparentSociety";
 
 const Page = () => {
   return (
     <main className="flex flex-col gap-20">
+      <TransparentSociety />
       <Content />
       <ContactInfo />
       <Waitlist />
